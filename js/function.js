64ce0cb6d73c1681d55fac9456e4952daced7bd1
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2018-11-16 17:06:14 -0200

    .

--- js/function.js
+++ js/function.js
@@ -7,18 +7,18 @@ $(document).ready(function(){
     //Scroll on click. NavBar
     $("a#navBox1").click(function(){ 
         $("html,body").animate({                         
-            scrollTop: $(".logoBack").offset().top},"slow");  
-    })
+            scrollTop: $("#logoBack").offset().top},"slow");  
+    });
     $("a#navBox2").click(function(){
         $("html,body").animate({                         
             scrollTop: $(".about").offset().top},"slow");  
-    })
+    });
     $("a#navBox3").click(function(){ 
         $("html,body").animate({                         
             scrollTop: $(".album").offset().top},"slow"); 
-    })
+    });
     $("a#navBox4").click(function(){
         $("html,body").animate({                         
             scrollTop: $(".socialMidia").offset().top},"slow"); 
-    })
+    });
 })
